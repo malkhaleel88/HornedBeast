481099b1181f6212a12cf3402b7f0313503f44ad
malkhaleel88 <m.alkhaleel@yahoo.com>
2021-07-25 22:54:40 +0300

    initial

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+
+    render() {
+        return(
+          <h3>Author : Mohammad AL-Khaleel</h3>
+        )
+      }
+
+}
+
+export default Footer;
